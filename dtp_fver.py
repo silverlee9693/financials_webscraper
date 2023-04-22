7891dff64fdef800da493b4e70b71895c7f960db
silverlee9693 <129355946+silverlee9693@users.noreply.github.com>
2023-04-22 13:37:49 +0800

    Update dtp_fver.py

--- dtp_fver.py
+++ dtp_fver.py
@@ -15,7 +15,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
 import codecs
 import re
 from webdriver_manager.chrome import ChromeDriverManager
